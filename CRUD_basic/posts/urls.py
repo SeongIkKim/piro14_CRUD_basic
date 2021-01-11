45e44678284389b587974bec106a0a966551dcd1
SeongIkKim <kpic1638@gmail.com>
2021-01-11 17:16:08 +0900

    complete post detail (R)

--- CRUD_basic/posts/urls.py
+++ CRUD_basic/posts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     # 대개 name은 view name과 같게 하여 헷갈림을 방지. 그러나 정확히는 해당 url path의 호출명을 의미함.
-    path(route='', view=views.post_list, name=''),
+    path(route='', view=views.post_list, name='list'),
+    path('<int:pk>/', views.post_detail, name='detail'),
 ]
--- CRUD_basic/posts/views.py
+++ CRUD_basic/posts/views.py
@@ -6,7 +6,25 @@ from django.shortcuts import render
 
 
 def post_list(request):
+    '''
+    Retrieve, Read 
+    저장된 모든 포스트를 렌더링하는 뷰
+    '''
     posts = Post.objects.all()  # 정확히는 여기가 Retrieve(Read)
     ctx = {'posts': posts}
 
     return render(request, template_name='posts/list.html', context=ctx)
+
+
+def post_detail(request, pk):
+    '''
+    Retrieve, Read
+    url 파라미터로 가져온 pk값에 해당하는 특정 포스트만을 렌더링하는 뷰
+    '''
+    # print(pk) # -> url에서 넘겨준 parameter
+    post = Post.objects.get(id=pk)  # 정확히는 여기가 Retrieve(Read)
+    # print(dir(post)) # -> post의 내부 변수들을 보여준다
+    # print(post.id) # -> 해당 post의 id는 (당연히) pk와 같다.
+    ctx = {'post': post}
+
+    return render(request, template_name='posts/detail.html', context=ctx)
