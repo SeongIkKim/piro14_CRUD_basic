b034c582a12ab378c719c3a6aeb05142f29f7033
SeongIkKim <kpic1638@gmail.com>
2021-01-11 18:45:02 +0900

    complete post update (U)

--- CRUD_basic/posts/urls.py
+++ CRUD_basic/posts/urls.py
@@ -6,6 +6,8 @@ app_name = 'posts'
 urlpatterns = [
     # 대개 name은 view name과 같게 하여 헷갈림을 방지. 그러나 정확히는 해당 url path의 호출명을 의미함.
     path(route='', view=views.post_list, name='list'),
-    path('<int:pk>/', views.post_detail, name='detail'),
+    path('<int:post_id>/', views.post_detail, name='detail'),
     path('create/', views.create_post, name='create'),
+    path('<int:post_id>/update/', views.update_post, name='update'),
+
 ]
