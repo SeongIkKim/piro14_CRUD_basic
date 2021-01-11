3936f1702b89e6767b715ac31826885fd3ea6433
SeongIkKim <kpic1638@gmail.com>
2021-01-11 18:20:13 +0900

    complete post create, form (C)

--- CRUD_basic/posts/urls.py
+++ CRUD_basic/posts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # 대개 name은 view name과 같게 하여 헷갈림을 방지. 그러나 정확히는 해당 url path의 호출명을 의미함.
     path(route='', view=views.post_list, name='list'),
     path('<int:pk>/', views.post_detail, name='detail'),
+    path('create/', views.create_post, name='create'),
 ]
--- CRUD_basic/posts/views.py
+++ CRUD_basic/posts/views.py
@@ -1,13 +1,14 @@
+from .forms import PostForm
 from .models import Post
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here
 
 
 def post_list(request):
     '''
-    Retrieve, Read 
+    Retrieve, Read == SELECT
     저장된 모든 포스트를 렌더링하는 뷰
     '''
     posts = Post.objects.all()  # 정확히는 여기가 Retrieve(Read)
@@ -18,7 +19,7 @@ def post_list(request):
 
 def post_detail(request, pk):
     '''
-    Retrieve, Read
+    Retrieve, Read == SELECT
     url 파라미터로 가져온 pk값에 해당하는 특정 포스트만을 렌더링하는 뷰
     '''
     # print(pk) # -> url에서 넘겨준 parameter
@@ -28,3 +29,20 @@ def post_detail(request, pk):
     ctx = {'post': post}
 
     return render(request, template_name='posts/detail.html', context=ctx)
+
+
+def create_post(request):
+    '''
+    Create == INSERT
+    모델을 이용하여 새로운 인스턴스를 생성하고 DB에 저장하는 뷰
+    '''
+    if request.method == 'POST':
+        form = PostForm(request.POST)  # 유저가 전송해온 데이터(POST)를 채워넣은 form객체 생성
+        if form.is_valid():  # form의 각 필드에 대하여 유효성 검증
+            post = form.save()  # DB에 저장
+            return redirect('posts:list')
+    else:  # 일반적으로 GET방식
+        form = PostForm()
+        ctx = {'form': form}
+
+        return render(request, 'posts/create.html', ctx)
